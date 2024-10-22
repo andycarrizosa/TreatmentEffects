59a14543ea7e1e1e4a7db31bb9ecbc5e7ffa6a07
Andres Carrizosa <andres@abpartners.co>
2024-10-22 17:01:10 -0300

    try if can get shiny app to work

---
+++ R/TEShiny.R
@@ -0,0 +1,54 @@
+library(ggplot2)
+library(Cairo)   # For nicer ggplot2 output when deployed on Linux
+library(TreatmentEffects)
+library(parallel)
+
+ui <- fluidPage(
+  fluidRow(
+    column(width = 4, wellPanel(
+            textInput("mydat", "Data Path"),
+            textInput("DV", "Dependent Variable"),
+            textInput("treats", "Treatments"),
+            textInput("subgroups", "Subgroups", value=""),
+            textInput("model", "Model", value="linear"),
+            textInput("controls", "Controls", value=""),
+            numericInput("sims", "Simulations", value=1000),
+            radioButtons("comb", "Perm or Comb",
+                         c("perm", "comb"), selected="perm"),
+            sliderInput("clust", "Clusters", min=1, max=detectCores()-2, value=1, round=TRUE, step=1),
+            actionButton("go", "Go!")
+            )),
+    column(width = 5,
+           textOutput("text"),
+      plotOutput("plot1"),
+      tableOutput("table")
+    )
+  )
+)
+
+
+server <- function(input, output) {
+        
+    gg<-eventReactive(input$go, {
+           start<-Sys.time()
+           gg<-TreatmentEffects(data=read.csv(input$mydat), 
+                                        treats=input$treats, 
+                                        DV=input$DV, 
+                                        model = input$model, 
+                                        controls = input$controls, 
+                                        subgroups = input$subgroups, 
+                                        sims = input$sims, 
+                                        comb = input$comb, 
+                                        clust = input$clust)
+           end<-Sys.time()
+           gg[[5]]<-paste0("Estimation time= ", round(end-start,3), " seconds")
+           gg
+           })
+
+  output$plot1<-renderPlot({gg()[[4]]})
+  output$table<-renderTable({gg()[[2]]})
+  output$text<-renderText({gg()[[5]]})
+}
+
+
+shinyApp(ui, server)
