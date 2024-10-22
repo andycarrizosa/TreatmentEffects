82ed751c65a0e1a4fc1a634e7d31670b3c021f95
Andres Carrizosa <andres@abpartners.co>
2024-10-22 09:54:54 -0300

    Annotate code

--- R/TreatmentEffects.R
+++ R/TreatmentEffects.R
@@ -2,30 +2,36 @@
 RegModel<-function(data, treats, DV, model,
                   controls,subgroups){
 
-    if(controls[1]!=""){
-        RHS<-paste0(controls, collapse="+")
-        RHS<-paste0(treats,"+",RHS)
-    }else{
-        RHS<-treats
-    }
+        if(controls[1]!=""){
+                #set up model with controls
+                RHS<-paste0(controls, collapse="+")
+                RHS<-paste0(treats,"+",RHS)
+        }else{
+                #set up model without controls
+                RHS<-treats
+        }
 
-    if(subgroups[1]!=""){
-        groups<-paste0(subgroups, collapse="+")
-        RHS<-gsub(treats, paste0(treats, "*",groups),RHS)
-    }
+        if(subgroups[1]!=""){
+                #include interactions b/w treatment and subgroups if applicable
+                groups<-paste0(subgroups, collapse="+")
+                RHS<-gsub(treats, paste0(treats, "*",groups),RHS)
+        }
 
-    form<-as.formula(paste0(DV, "~", RHS))
+        # Turn text into a formula
+        form<-as.formula(paste0(DV, "~", RHS))
 
-    if(model=="linear"){
-        reg<-lm(form, data=data)
-    } else if(model=="logit"){
-        reg<-glm(form, data=data, family = "binomial")
-    }else stop("Please specify model. Options are 'linear' or 'logit'.")
+        if(model=="linear"){
+                reg<-lm(form, data=data) # Run linear model if specified
+        } else if(model=="logit"){
+                reg<-glm(form, data=data, family = "binomial") # Run logit model if specified
+        }else stop("Please specify model. Options are 'linear' or 'logit'.") #otherwise return error
 
     return(reg)
 }
 
 SimModel<-function(data, reg, treats, subgroups,sims, comb, clust){
+        # Set up all conditions to vary for simulations
+        # We will vary treatment conditions and if applicable we will also vary subgroup conditions
         treatlist<-list()
         treatlist[[treats]]<-data[treats] %>% unlist %>% unique
 
@@ -36,19 +42,21 @@ SimModel<-function(data, reg, treats, subgroups,sims, comb, clust){
                 }
         }
 
-        #Run simulations
+        #Run simulations for treatment and if applicable also subgroup conditions
         if(clust<2){
+                #run simulations in a single cluster
                 s<-clarify::sim(reg, n=sims)
                 est <- sim_setx(s, x = treatlist,
                                 verbose = TRUE)
-                #get estimated bootstraps
                 boots<-est %>% data.frame
+
         }else{
+                #run simulations as a parallel process through multiple clusters to speed estimation
                 cl<-makeCluster(clust)
                 registerDoParallel(cl)
                 options(cores=clust)
+                minisims<-ceiling(sims/clust)
 
-                minisims<-sims/clust
                 est<-foreach(i=1:length(clust))%dopar%{
                         library(clarify)
                         s<-clarify::sim(reg, n=minisims)
@@ -56,6 +64,7 @@ SimModel<-function(data, reg, treats, subgroups,sims, comb, clust){
                                         verbose = TRUE)
                         return(est)
                 }
+
                 boots<-lapply(est, data.frame) %>%
                         do.call(rbind, .)
                 stopCluster(cl)
@@ -65,8 +74,8 @@ SimModel<-function(data, reg, treats, subgroups,sims, comb, clust){
 
 FirstDifference<-function(data,boots, comb, treats, subgroups){
         FD<-list()
-
         if(subgroups[1]==""){
+                # take first difference between all estimated bootstrap columns to get differences between all treatments
                 if(comb=="comb"){
                         combs<-combinations(n=ncol(boots), r=2)
                 } else if(comb=="perm"){
@@ -85,6 +94,7 @@ FirstDifference<-function(data,boots, comb, treats, subgroups){
         }
 
         if(subgroups[1]!=""){
+                # first subset by each subgroup category, and then perform first differences of all treatments within each category
                 sublevel<-data[subgroups] %>% unlist %>% unique %>% gsub(" ", ".",.) %>%
                         gsub("[-]", ".",.) %>% gsub("[+]",".",.)
                 for(n in 1:length(sublevel)){
@@ -123,6 +133,7 @@ SummarizeData<-function(FD, treats, DV, subgroups){
         CI85<-sapply(FD, quantile, c(.075, .5, .925)) %>% t
 
         if(subgroups!=""){
+                # clean data and create a variable for each subgroup of the subgroup analysis
                 ph<-strsplit(rownames(CI95), "[:]") %>% data.frame %>% t %>% trimws()
                 CI95<-cbind(data.frame(CI95), data.frame(ph))
                 CI95$confint<-"95% Interval"
@@ -135,6 +146,7 @@ SummarizeData<-function(FD, treats, DV, subgroups){
                 names(CI85)<-c("min", "med", "max", "group", "treat", "conf")
                 lowsignif<-(CI85$max<0 | CI85$min>0)
         } else{
+                #clean data without subgroup variable (not applicable)
                 ph<-rownames(CI95) %>% trimws()
                 CI95<-cbind(data.frame(CI95), data.frame(ph))
                 CI95$confint<-"95% Interval"
@@ -163,7 +175,7 @@ SummarizeData<-function(FD, treats, DV, subgroups){
 PlotEffects<-function(clean_df, treats, DV, model,
                       controls,subgroups,sims){
         if(subgroups[1]==""){
-                #plot
+                #plot figure if subgroups do not exist
                 gg<-ggplot(clean_df, aes(x=treat, ymin=min, y=med, ymax=max))+
                         geom_pointrange(lwd=2,size=1,  aes(col=conf, pch=signif_dif))+
                         geom_hline(yintercept=0, col="red", lty=2)+
@@ -178,7 +190,7 @@ PlotEffects<-function(clean_df, treats, DV, model,
                              caption=paste0("function:", "OneClarifyComb", "; model: ", model, "; treats: ", treats, ";\n controls: ", paste0(controls, collapse=", "), "; subgroups: ", subgroups, "; sims: ", sims))
 
         } else{
-
+                # plot figure if subgroups do exist
                 gg<-ggplot(clean_df, aes(x=treat, ymin=min, y=med, ymax=max))+
                         geom_pointrange(lwd=2,size=1,  aes(col=conf, pch=signif_dif))+
                         facet_wrap(.~group)+
@@ -210,22 +222,19 @@ TreatmentEffects<-function(data, treats="", DV="", model=c("linear", "logit"),
         library(doParallel)
         library(foreach)
 
-        start<-Sys.time()
-        # perform the regression
-        reg<-RegModel(data,treats,DV,model,controls,subgroups)
-        boots<-SimModel(data, reg, treats, subgroups,sims,comb, clust)
-        FD<-FirstDifference(data,boots, comb, treats, subgroups)
-        clean_df<-SummarizeData(FD, treats, DV, subgroups)
-        # plot the results of the simulations
-        gg<-PlotEffects(clean_df,treats,DV,model,controls,subgroups,sims)
-        #detail specification so people can easily report what they did
-        specification<-c("treats", "DV", "model", "controls", "subgroups","sims","function",
-                         treats, DV, model, paste0(controls, collapse=", "), subgroups, sims,"OneClarifyComb") %>% matrix(ncol=2, nrow=7)
+        reg<-RegModel(data,treats,DV,model,controls,subgroups) # run the regression model
+        boots<-SimModel(data, reg, treats, subgroups,sims,comb, clust) #perform the simulations
+        FD<-FirstDifference(data,boots, comb, treats, subgroups) # get differences between bootstraps of simulations
+        clean_df<-SummarizeData(FD, treats, DV, subgroups) #summarize by gettintg quantiles and median of each bootstrapped first-difference
+        gg<-PlotEffects(clean_df,treats,DV,model,controls,subgroups,sims) #plot simplified data
+
+        # Return summary information about how the model was specified
+        specification<-c("treats", "DV", "model", "controls", "subgroups","sims",
+                         treats, DV, model, paste0(controls, collapse=", "), subgroups, sims) %>% matrix(ncol=2, nrow=6)
         colnames(specification)<-c("argument", "user-specified")
 
         out<-list("reg"=reg, "clean_df"=clean_df, "specification"=specification, "gg"=gg)
 
-        end<-Sys.time()
-
         return(out)
+        end<-Sys.time()
 }
