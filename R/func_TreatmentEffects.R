81fbcd9dc2ca79b19ca8d82bd363f3128528eede
Andres Carrizosa <andres@abpartners.co>
2024-10-23 15:44:49 -0300

    update description and namespace

--- R/func_TreatmentEffects.R
+++ R/func_TreatmentEffects.R
@@ -1,15 +1,3 @@
-library(stringr)
-library(Hmisc)
-library(ggplot2)
-library(clarify)
-library(tidyverse)
-library(RColorBrewer)
-library(gtools)
-library(doParallel)
-library(foreach)
-library(shiny)
-library(knitr)
-library(rmarkdown)
 
 RegModel<-function(data, treats, DV, model,
                   controls,subgroups){
@@ -224,18 +212,6 @@ PlotEffects<-function(clean_df, treats, DV, model,
 TreatmentEffects<-function(data, treats="", DV="", model=c("linear", "logit"),
                            controls="",subgroups="",sims=10000,
                            comb="perm", clust=1){
-        library(stringr)
-        library(Hmisc)
-        library(ggplot2)
-        library(clarify)
-        library(tidyverse)
-        library(RColorBrewer)
-        library(gtools)
-        library(doParallel)
-        library(foreach)
-        library(shiny)
-        library(knitr)
-        library(rmarkdown)
 
         reg<-RegModel(data,treats,DV,model,controls,subgroups) # run the regression model
         boots<-SimModel(data, reg, treats, subgroups,sims,comb, clust) #perform the simulations
